b60f0e78d871ff27989c3e08291497854ecc42a7
drGoldMember <tomasz.cimicki@gmail.com>
2026-04-02 12:59:01 +0200

    Updated TODO

--- plugins/top-bar/src/components/AdminBarView.vue
+++ plugins/top-bar/src/components/AdminBarView.vue
@@ -12,6 +12,8 @@
 
 // Do przemyslenia
 - Dodac tooltipy do buttonow
+- Gdy ustawie dwa TopBary w pozycji TOP, jeden nachodzi na drugi. dobrze by bylo aby jeden byl pod drugim itp. To samo w pozycji bottom
+- Moze dodac opcje: zamykania recznego lub po czasie dla danego TopBar ? Wtedy jeden mozna zrobic statycznie, drugi pod spodem znikal by po np 30sek 
 
 BUGI
 - Gdy usuwam bar z Scheduled wywala blad : An error occurred
