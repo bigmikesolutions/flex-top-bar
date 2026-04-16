53a7d4924976c654ad6b694a9934caaed66c8e9c
Michal Wronski <michal.robert.wronski@gmail.com>
2026-04-16 09:24:39 +0200

    minor

--- plugins/flex-top-bar/tests/FeatureFlagsTest.php
+++ plugins/flex-top-bar/tests/FeatureFlagsTest.php
@@ -11,6 +11,9 @@ namespace FlexTopBar\Tests;
 
 use PHPUnit\Framework\TestCase;
 use FlexTopBar\FeatureFlags;
+use FlexTopBar\FreePlan;
+use FlexTopBar\PlanNameProvider;
+use FlexTopBar\ProPlan;
 
 final class FeatureFlagsTest extends TestCase {
 
@@ -27,12 +30,13 @@ final class FeatureFlagsTest extends TestCase {
 	public function test_defaults_when_no_constants_defined(): void {
 		FeatureFlags::reset_for_tests();
 		$flags = FeatureFlags::instance();
+		$free  = new FreePlan();
 
-		$this->assertSame( 1, $flags->max_bars() );
-		$this->assertSame( 1, $flags->max_messages() );
-		$this->assertSame( 1, $flags->max_columns() );
-		$this->assertFalse( $flags->schedule_enabled() );
-		$this->assertSame( 'free', $flags->plan_name() );
+		$this->assertSame( $free->max_bars(), $flags->max_bars() );
+		$this->assertSame( $free->max_messages(), $flags->max_messages() );
+		$this->assertSame( $free->max_columns(), $flags->max_columns() );
+		$this->assertSame( $free->schedule_enabled(), $flags->schedule_enabled() );
+		$this->assertSame( $free->plan_name(), $flags->plan_name() );
 	}
 
 	/**
@@ -45,12 +49,13 @@ final class FeatureFlagsTest extends TestCase {
 		}
 		FeatureFlags::reset_for_tests();
 		$flags = FeatureFlags::instance();
+		$pro   = new ProPlan();
 
-		$this->assertSame( 5, $flags->max_bars() );
-		$this->assertSame( 5, $flags->max_messages() );
-		$this->assertSame( 4, $flags->max_columns() );
-		$this->assertTrue( $flags->schedule_enabled() );
-		$this->assertSame( 'pro', $flags->plan_name() );
+		$this->assertSame( $pro->max_bars(), $flags->max_bars() );
+		$this->assertSame( $pro->max_messages(), $flags->max_messages() );
+		$this->assertSame( $pro->max_columns(), $flags->max_columns() );
+		$this->assertSame( $pro->schedule_enabled(), $flags->schedule_enabled() );
+		$this->assertSame( $pro->plan_name(), $flags->plan_name() );
 	}
 
 	/**
@@ -65,6 +70,57 @@ final class FeatureFlagsTest extends TestCase {
 		$flags = FeatureFlags::instance();
 
 		$this->assertSame( 'free', $flags->plan_name() );
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
+	public function test_ff_plan_is_case_insensitive_and_trimmed(): void {
+		if ( ! defined( 'FF_PLAN' ) ) {
+			define( 'FF_PLAN', '  PRO  ' );
+		}
+		FeatureFlags::reset_for_tests();
+		$flags = FeatureFlags::instance();
+
+		$this->assertSame( 'pro', $flags->plan_name() );
+		$this->assertTrue( $flags->schedule_enabled() );
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
+	public function test_unknown_ff_plan_defaults_to_free(): void {
+		if ( ! defined( 'FF_PLAN' ) ) {
+			define( 'FF_PLAN', 'enterprise' );
+		}
+		FeatureFlags::reset_for_tests();
+		$flags = FeatureFlags::instance();
+
+		$this->assertSame( 'free', $flags->plan_name() );
+		$this->assertFalse( $flags->schedule_enabled() );
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
+	public function test_non_string_ff_plan_is_ignored_and_provider_is_used(): void {
+		if ( ! defined( 'FF_PLAN' ) ) {
+			define( 'FF_PLAN', 123 );
+		}
+
+		$provider = new class() implements PlanNameProvider {
+			public function get_plan_name(): string {
+				return 'pro';
+			}
+		};
+
+		$flags = new FeatureFlags( $provider );
+
+		$this->assertSame( 'pro', $flags->plan_name() );
+		$this->assertTrue( $flags->schedule_enabled() );
 	}
 
 	public function test_singleton_returns_same_instance(): void {
